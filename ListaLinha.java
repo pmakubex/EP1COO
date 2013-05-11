4f8f6042787f2cde0f8307f6472c3eaacc6c7847
gukiboy <gustavokishima@gmail.com>
2013-05-11 00:12:56 -0300

    Update ListaLinha.java

--- ListaLinha.java
+++ ListaLinha.java
@@ -7,15 +7,16 @@ class ListaLinha{
 		fim = null;
 	}
 
-	void adicionaLinha(int num, String cont){
+	void adicionaLinha(int num, String cont, String nomeArq){
 		if(inicio == null){//Lista vazia
-			inicio = new LinhaDeArquivo(num,cont);
+			inicio = new LinhaDeArquivo(num,cont,nomeArq);
 			fim = inicio;
 			return;
 		}
 	
 		fim.proximo() = new LinhaDeArquivo(num,cont);
 		fim = fim.proximo();
+		return;
 	}
 
 }
